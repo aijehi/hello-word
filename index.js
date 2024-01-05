7a82114470dacd4c21c0becb465341abc65d87a2
aijehi <155052907+aijehi@users.noreply.github.com>
2024-01-05 14:30:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World. Ths is test edit aston martin!');
+  res.send('Hello, World. Ths is test edit aston martinS!');
 });
 
 app.listen(port, () => {
